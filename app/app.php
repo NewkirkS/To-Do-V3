7646ebc914779b3cd48b52c10dddae605f138b23
Aimen Khakwani and Ayana Powell <ayana@>
2016-09-20 09:39:12 -0700

    Add user interface

--- app/app.php
+++ app/app.php
@@ -18,21 +18,26 @@
     ));
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.html.twig');
+    return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
     });
 
     $app->get("/tasks", function() use ($app) {
         return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
     });
 
-    $app->get("/categories", function() use ($app) {
-        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    $app->get("/categories/{id}", function($id) use ($app) {
+        $category = Category::find($id);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
     });
 
-    $app->post("/tasks", function() use ($app){
-        $task = new Task($_POST['description']);
+
+    $app->post("/tasks", function() use ($app) {
+        $description = $_POST['description'];
+        $category_id = $_POST['category_id'];
+        $task = new Task($description, $id = null, $category_id);
         $task->save();
-        return $app['twig']->render('tasks.html.twig', array('tasks' =>  Task::getAll()));
+        $category = Category::find($category_id);
+        return $app['twig']->render('category.html.twig', array('category' => $category, 'tasks' => $category->getTasks()));
     });
 
     $app->post("/categories", function() use ($app) {
